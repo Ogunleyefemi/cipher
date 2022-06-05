ff4bb752c93b3716d9afda547adcef39c2483461
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-06-05 12:36:00 +0100

    add functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,58 @@
-let y = prompt("input a sentence");
-function sentence(){
-    return y;
-}
-console.log(sentence());
+let sentence = prompt("input a sentence");
+console.log();
 //taking the first and the last letter
-function sentence(y){
-    let firstcharacter = y.charAt(0).toUpperCase();
-    let lastcharacter = y.charAt(y.length -1).toUpperCase();
-    let concat1 = firstcharacter.concat(lastcharacter);
-    return concat1;
-    document.write(sentence());
+// function character(sentence){
+    //to get the first letter.
+    let firstcharacter = sentence.charAt(0).toUpperCase();
+    //to get the second letter.
+    let lastcharacter = sentence.charAt(sentence.length -1).toUpperCase();
+    // function to take first letter and last letter and rtn as a str in cap.letter
+    let concat = firstcharacter.concat(lastcharacter);
+    function firstandlastletter(){
+        return concat;
+    }
+alert(firstandlastletter());
+
+//to reverse my first and last letter
+let rev = concat.split("");
+function splitFirstAndLast(){
+    return rev;
 }
-let splitstring = .split("concat1")
+alert(splitFirstAndLast());
+//reversing split
+let splitArray = rev.reverse("");
+function reverseSplit(){
+    return splitArray;
+}
+alert(reverseSplit());
+//joining reverse
+let joinReverse = splitArray.join("");
+function joinArray(){
+    let joinReverse = splitArray.join("");
+    return joinReverse;
+}
+alert(joinArray());
+//function that call my first two functions
+function thirdfunction(){
+    let firstfunction = concat;
+    let secondfunction = joinReverse;
+    let thirdfunction = sentence.concat(joinReverse);
+    return thirdfunction;
+}
+alert(thirdfunction());
+
+
+//function to count numbers of characters in the sentence.
+let total = sentence.length("");
+function divideByTwo(){
+    let answer = total / 2;
+    return answer;
+}
+alert(divideByTwo());
+
+
+
+
+
+
+
